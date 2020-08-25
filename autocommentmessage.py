93c637154e2da25e0deb8a4a2fbd57248c6ba4fe
Tousif-Mahmud-Emon <61532232+Tousif-Mahmud-Emon@users.noreply.github.com>
2020-08-25 22:21:46 +0600

    Create autocommentmessage.py

---
+++ autocommentmessage.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+
+A = [" "] #inside "" write message/comment which you want to do 
+
+time.sleep(5)
+
+for i in range():  #inside() set range
+    pyautogui.typewrite(A[i]) 
+    pyautogui.typewrite("\n")
+    time.sleep(2) #set time inside()
